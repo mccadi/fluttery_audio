a78a85bcbed12546610564a4848814fc87355f89
Michael Cadigan <michael@numo.com>
2019-07-17 23:39:51 -0400

    fixes errors

--- lib/fluttery_audio.dart
+++ lib/fluttery_audio.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttery_audio/src/_audio_player.dart';
 import 'package:fluttery_audio/src/_audio_visualizer.dart';
@@ -9,12 +10,57 @@ export 'src/_audio_visualizer.dart';
 export 'src/_playlist.dart';
 export 'src/_visualizer.dart';
 
-class FlutteryAudio {
-  static const MethodChannel _channel =
-      const MethodChannel('fluttery_audio');
+class Radi8erPlaylist {
+  Radi8erPlaylist() {
+    songs = List<Song>();
+  }
+  List<Song> songs;
+  static Radi8erPlaylist fromResponse(Response response) {
+    Iterable l = response.data['data'];
+    print(l.toString());
+    return Radi8erPlaylist()..songs = l.map((model) => Song.fromJson(model)).toList();
+  }
+}
 
-  static const MethodChannel _visualizerChannel =
-      const MethodChannel('fluttery_audio_visualizer');
+class Song {
+  String artUrl;
+  String artFullUrl;
+  String artMediumUrl;
+  String artist;
+  String songTitle;
+  String artThumbNailUrl;
+  int artistId;
+  String contribute;
+  int id;
+  String merch;
+  bool seed;
+  String url;
+  String website;
+  String albumName;
+
+  static Song fromJson(json) {
+    return Song()
+      ..albumName = json["album"]
+      ..artFullUrl = json["art_full"]
+      ..artMediumUrl = json["art_mid"]
+      ..artThumbNailUrl = json["art_thumb"]
+      ..artUrl = json["art_url"]
+      ..artist = json["artist"]
+      ..artistId = json["artist_id"]
+      ..songTitle = json["title"]
+      ..contribute = json["contribute"]
+      ..id = json["id"]
+      ..merch = json["merch"]
+      ..seed = json["seed"]
+      ..url = json["url"]
+      ..website = json["website"];
+  }
+}
+
+class FlutteryAudio {
+  static const MethodChannel _channel = const MethodChannel('fluttery_audio');
+
+  static const MethodChannel _visualizerChannel = const MethodChannel('fluttery_audio_visualizer');
 
   static AudioPlayer audioPlayer() {
     return new AudioPlayer(
--- lib/src/_playlist.dart
+++ lib/src/_playlist.dart
@@ -1,58 +1,11 @@
 import 'package:flutter/widgets.dart';
+import 'package:fluttery_audio/fluttery_audio.dart';
 import 'package:fluttery_audio/src/_audio_player.dart';
 import 'package:fluttery_audio/src/_audio_player_widgets.dart';
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
-import 'package:dio/dio.dart';
 
 final _log = new Logger('AudioPlaylist');
-
-class Radi8erPlaylist {
-  Radi8erPlaylist() {
-    songs = List<Song>();
-  }
-  List<Song> songs;
-  static Radi8erPlaylist fromResponse(Response response) {
-    Iterable l = response.data['data'];
-    print(l.toString());
-    return Radi8erPlaylist()..songs = l.map((model) => Song.fromJson(model)).toList();
-  }
-}
-
-class Song {
-  String artUrl;
-  String artFullUrl;
-  String artMediumUrl;
-  String artist;
-  String songTitle;
-  String artThumbNailUrl;
-  int artistId;
-  String contribute;
-  int id;
-  String merch;
-  bool seed;
-  String url;
-  String website;
-  String albumName;
-
-  static Song fromJson(json) {
-    return Song()
-      ..albumName = json["album"]
-      ..artFullUrl = json["art_full"]
-      ..artMediumUrl = json["art_mid"]
-      ..artThumbNailUrl = json["art_thumb"]
-      ..artUrl = json["art_url"]
-      ..artist = json["artist"]
-      ..artistId = json["artist_id"]
-      ..songTitle = json["title"]
-      ..contribute = json["contribute"]
-      ..id = json["id"]
-      ..merch = json["merch"]
-      ..seed = json["seed"]
-      ..url = json["url"]
-      ..website = json["website"];
-  }
-}
 
 class AudioPlaylist extends StatefulWidget {
   final List<String> playlist;
